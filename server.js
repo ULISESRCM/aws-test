2ee2a1c0061a63c6b3d19940f515a2367e5e7e43
ulsiesrcm <ulisesrcm001@gmail.com>
2024-07-11 15:13:20 -0300

    texto modificado

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req,res) => res.send('Hello world with express'))
+app.get('/', (req,res) => res.send('<h1>Hello world with express</h1>'))
 
 app.listen(3000);
 console.log('server on port 3000');
